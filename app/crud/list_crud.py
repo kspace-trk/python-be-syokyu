3bc9682de7650bbefaeba501f878b4f268ccf448
kspace-trk <keigonakao.0825@gmail.com>
2026-08-26 13:59:35 +0900

    DbSession周り修正

--- app/crud/list_crud.py
+++ app/crud/list_crud.py
@@ -1,10 +1,9 @@
 """TODOリストのDB操作."""
 
-from sqlalchemy.orm import Session
-
+from app.dependencies import DbSession
 from app.models.list_model import ListModel
 
 
-def get_todo_list(db: Session, todo_list_id: int) -> ListModel | None:
+def get_todo_list(db: DbSession, todo_list_id: int):
     """TODOリストを1件取得する."""
     return db.query(ListModel).filter(ListModel.id == todo_list_id).first()
