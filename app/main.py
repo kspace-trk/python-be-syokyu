b82277247a5936f8dae64cbe5764d0429350bd7d
kspace-trk <keigonakao.0825@gmail.com>
2026-08-26 10:56:14 +0900

    schemaだけ分割

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import os
-from datetime import datetime
 
 from fastapi import FastAPI
-from pydantic import BaseModel, ConfigDict, Field
 
 from app.const import TodoItemStatusCode
+from app.schemas.item_schema import NewTodoItem, ResponseTodoItem, UpdateTodoItem
+from app.schemas.list_schema import NewTodoList, ResponseTodoList, UpdateTodoList
 
 from .models.item_model import ItemModel
 from .models.list_model import ListModel
@@ -27,61 +27,6 @@ if DEBUG:
         panels=["app.database.SQLAlchemyPanel"],
     )
 
-
-class NewTodoItem(BaseModel):
-    """TODO項目新規作成時のスキーマ."""
-
-    title: str = Field(title="Todo Item Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo Item Description", min_length=1, max_length=200)
-    due_at: datetime | None = Field(default=None, title="Todo Item Due")
-
-
-class UpdateTodoItem(BaseModel):
-    """TODO項目更新時のスキーマ."""
-
-    title: str | None = Field(default=None, title="Todo Item Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo Item Description", min_length=1, max_length=200)
-    due_at: datetime | None = Field(default=None, title="Todo Item Due")
-    complete: bool | None = Field(default=None, title="Set Todo Item status as completed")
-
-
-class ResponseTodoItem(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-
-    id: int
-    todo_list_id: int
-    title: str = Field(title="Todo Item Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo Item Description", min_length=1, max_length=200)
-    status_code: TodoItemStatusCode = Field(title="Todo Status Code")
-    due_at: datetime | None = Field(default=None, title="Todo Item Due")
-    created_at: datetime = Field(title="datetime that the item was created")
-    updated_at: datetime = Field(title="datetime that the item was updated")
-
-
-class NewTodoList(BaseModel):
-    """TODOリスト新規作成時のスキーマ."""
-
-    title: str = Field(title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
-
-
-class UpdateTodoList(BaseModel):
-    """TODOリスト更新時のスキーマ."""
-
-    title: str | None = Field(default=None, title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
-
-
-class ResponseTodoList(BaseModel):
-    """TODOリストのレスポンススキーマ."""
-
-    model_config = ConfigDict(from_attributes=True)
-
-    id: int
-    title: str = Field(title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
-    created_at: datetime = Field(title="datetime that the item was created")
-    updated_at: datetime = Field(title="datetime that the item was updated")
 
 @app.get("/echo", tags=["Echo"])
 def get_echo(message: str, name: str):
